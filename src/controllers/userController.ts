abe677a382bee1b238e26e37f40725659828e18b
tranhuyviet <vietth@msn.com>
2021-12-03 12:45:01 +0200

    __

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -63,7 +63,7 @@ export const signup = async (
             maxAge: 60 * 60 * 24 * 30,
             sameSite: 'none',
             path: '/',
-            domain: 'http://localhost:3000',
+            domain: 'https://fs8-project.vercel.app',
         })
 
         // return user with authentication
@@ -135,7 +135,7 @@ export const login = async (
             maxAge: 60 * 60 * 24 * 30,
             sameSite: 'none',
             path: '/',
-            domain: 'http://localhost:3000',
+            domain: 'https://fs8-project.vercel.app',
         })
 
         // return user with authentication
@@ -164,7 +164,7 @@ export const logout = (req: Request, res: Response, next: NextFunction) => {
             expires: new Date(0),
             sameSite: 'none',
             path: '/',
-            domain: 'http://localhost:3000',
+            domain: 'https://fs8-project.vercel.app',
         })
         return resSuccess(res)
     } catch (error) {
@@ -270,7 +270,7 @@ export const changePassword = async (
             maxAge: 60 * 60 * 24 * 30,
             sameSite: 'none',
             path: '/',
-            domain: 'http://localhost:3000',
+            domain: 'https://fs8-project.vercel.app',
         })
 
         // return user with authentication
@@ -373,7 +373,7 @@ export const forgotPassword = async (
         await userService.save(user)
 
         // const resetUrl = `http://localhost:5001/api/v1/users/reset-password/${resetToken}`
-        const resetUrl = `http://localhost:3000/user/reset-password/${resetToken}`
+        const resetUrl = `https://fs8-project.vercel.app/user/reset-password/${resetToken}`
 
         // sent the email
         try {
